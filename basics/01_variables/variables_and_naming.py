15bc259fe63683c7152e904c874cf408f6d3cd9e
Mouhcine96 <mouhcine.amjahid@gmail.com>
2026-01-13 19:31:32 +0100

    Basics: variables, naming conventions, and reassignment

---
+++ basics/01_variables/variables_and_naming.py
@@ -0,0 +1,22 @@
+# Variable declaration
+user_name = "Mouhcine Amjahid"
+user_age = 29
+
+user_age = user_age + 1  # Incrementing age by 1
+
+print(user_name)
+print(user_age)
+
+# Case sensitivity example
+age = 30
+Age = 40
+
+print(age)
+print(Age)
+
+# Descriptive variable names
+account_balance = 1500.75
+is_logged_in = True
+
+print(account_balance)
+print(is_logged_in)
