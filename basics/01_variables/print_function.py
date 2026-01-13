0f77c3f2a27f8bbcfc9d9b42e60b71afb5093cb7
Mouhcine96 <mouhcine.amjahid@gmail.com>
2026-01-13 19:14:43 +0100

    Basics: using the print function

---
+++ basics/01_variables/print_function.py
@@ -0,0 +1,12 @@
+# Basic print examples
+print("Hello world!")
+
+# Printing variables
+user_name = "Mouhcine"
+user_age = 29
+
+print("Name:", user_name)
+print("Age:", user_age)
+
+# Printing multiple values
+print("My favorite colors are", "blue", "green", "red", "and", "black")
